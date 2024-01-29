5976113b6aa1aaff76357598f08e0e81003dd55b
Daniel Vargas <daniel.rvargas10@gmail.com>
2024-01-28 22:57:50 -0300

    style: finished frontend

--- src/pages/js/style.js
+++ src/pages/js/style.js
@@ -1,26 +1,32 @@
 const nextBtn = document.getElementById("next-btn");
 const previousBtn = document.getElementById("previous-btn");
 const actionPage = document.querySelector(".action-page");
+const printBtn = document.getElementById("print-btn");
 
 nextBtn.addEventListener("click", () => {
   const fileSpan = document.getElementById("new-placeholder");
 
   if (fileSpan.innerHTML) {
-    actionPage.setAttribute(
-      "style",
-      "transform: translateX(-100%);transition: 0.5s;"
-    );
+    actionPage.style.transform = "translateX(-100%)";
+    actionPage.style.transition = "0.5s";
   } else {
-    inputSearch.setAttribute(
-      "style",
-      "border:2px solid red;animation: shake-horizontal 0.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) both;"
-    );
+    inputSearch.style.border = "2px solid red";
+    inputSearch.style.animation =
+      "shake 0.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) both";
   }
 });
 
 previousBtn.addEventListener("click", () => {
-  actionPage.setAttribute(
-    "style",
-    "transform: translateX(100%);transition: 0.5s;"
-  );
+  actionPage.style.transform = "translateX(100%)";
+  actionPage.style.transition = "0.5s";
+});
+
+printBtn.addEventListener("click", () => {
+  printBtn.style.backgroundColor = "#00E500";
+  printBtn.style.transition = "1s";
+});
+
+printBtn.addEventListener("blur", () => {
+  printBtn.style.backgroundColor = "blueviolet";
+  printBtn.style.transition = "1s";
 });
