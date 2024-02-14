e7d3c90a350be554f76b30031c6255b6f7f7a012
Daniel Vargas <daniel.rvargas10@gmail.com>
2024-02-14 14:59:27 -0300

    v4.02.06

--- install.bat
+++ install.bat
@@ -1,52 +1,4 @@
 @echo off
-:init
-setlocal DisableDelayedExpansion
-set cmdInvoke=1
-set winSysFolder=System32
-set "batchPath=%~0"
-for %%k in (%0) do set batchName=%%~nk
-set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
-setlocal EnableDelayedExpansion
-
-:checkPrivileges
-NET FILE 1>NUL 2>NUL
-if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
-
-:getPrivileges
-if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
-echo.
-echo **************************************
-echo Invoking UAC for Privilege Escalation
-echo **************************************
-
-echo Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
-echo args = "ELEV " >> "%vbsGetPrivileges%"
-echo For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
-echo args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
-echo Next >> "%vbsGetPrivileges%"
-
-if '%cmdInvoke%'=='1' goto InvokeCmd 
-
-echo UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
-goto ExecElevation
-
-:InvokeCmd
-echo args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
-echo UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
-
-:ExecElevation
-"%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
-exit /B
-
-:gotPrivileges
-setlocal & cd /d %~dp0
-if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
-
-::::::::::::::::::::::::::::
-::START
-::::::::::::::::::::::::::::
-
-@echo off
 title ImprimePDF
 cd resource
 
@@ -102,10 +54,11 @@ IF errorlevel=1 goto SIM
 :SIM
 echo O usuario pressionou [S]
 echo.
-rmdir /s /q "C:/Program Files/ImprimePDF"
+rmdir /s /q "C:\Users\%USERNAME%\AppData\Roaming\ImprimePDF"
 cd ..
-Xcopy /E /I resource "C:\Program Files\ImprimePDF"
-cd C:/Program Files/ImprimePDF
+Xcopy /E /I %CD%\resource C:\Users\%USERNAME%\AppData\Roaming\ImprimePDF
+cd C:\Users\%USERNAME%\AppData\Roaming\ImprimePDF
+copy %CD%\ImprimePDF.lnk \\metaro-server\UsuariosAD\%USERNAME%\Desktop
 echo.
 
     choice /C SN /M "Instalar Dependencias?
@@ -114,7 +67,7 @@ echo.
     :SIM
     echo O usuario pressionou [S]
     echo.
-    npm install && start explorer "C:\Program Files\ImprimePDF"
+    npm install
     goto fim3
     :NAO
     echo O usuario pressionou [N]
